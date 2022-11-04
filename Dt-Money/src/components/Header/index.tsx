8c1de75a83209b15070a07fee9ac5b87d003076b
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-11-04 13:22:40 -0300

    modified:   Dt-Money/package-lock.json modified:   Dt-Money/package.json modified:   Dt-Money/src/components/Header/index.tsx new file:   Dt-Money/src/components/NewTransactionModal/index.tsx new file:   Dt-Money/src/components/NewTransactionModal/styles.ts new file:   Dt-Money/src/components/SearchForm/index.tsx new file:   Dt-Money/src/components/SearchForm/styles.ts modified:   Dt-Money/src/pages/Transactions/index.tsx modified:   Dt-Money/src/pages/Transactions/styles.ts

--- Dt-Money/src/components/Header/index.tsx
+++ Dt-Money/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 ﻿import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import logoImg from '../../assets/MoneyIcon.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -8,7 +10,13 @@ export function Header() {
         <img src={logoImg} alt="Imagem de saco de dinheiro azul claro" />
         <h1>Controle de finanças</h1>
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
