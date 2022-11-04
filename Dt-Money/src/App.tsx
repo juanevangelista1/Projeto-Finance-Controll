49ff17a949b0b1798b5f6f6d2ab9c89c85f31710
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-11-04 18:20:23 -0300

    Criação de Contexto

--- Dt-Money/src/App.tsx
+++ Dt-Money/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
+import { TransactionsProvider } from './contexts/TransactionsContext'
 import { Transactions } from './pages/Transactions'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -8,7 +9,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
