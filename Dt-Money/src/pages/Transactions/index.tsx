882d99ab18032d162e422381a9af880bbbac9604
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-11-04 14:38:07 -0300

    Update da porta do JSON server

--- Dt-Money/src/pages/Transactions/index.tsx
+++ Dt-Money/src/pages/Transactions/index.tsx
@@ -21,7 +21,7 @@ export function Transactions() {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
   async function loadTransactions() {
-    const response = await fetch('http://localhost:3000/transactions')
+    const response = await fetch('http://localhost:3333/transactions')
     const data = await response.json()
 
     setTransactions(data)
